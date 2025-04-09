16bdc6e72fba70bd421163c8b469bf79d84d8fdf
TJ Bai <tjianbai@gmail.com>
2025-04-08 23:18:19 -0400

    nice charts, tvd not getting as low as reference

--- forge/ising.py
+++ forge/ising.py
@@ -1,21 +1,27 @@
+from typing import List
+from itertools import product
+
 import torch
 import torch.nn.functional as F
+import numpy as np
+import matplotlib.pyplot as plt
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 VOCAB = torch.tensor([[[-1., 1.]]], device=device)
+V = VOCAB.shape[-1]
 
 def ncycle_energy(state: torch.Tensor, beta: float):
-    return -beta * torch.sum(state * torch.roll(state, shifts=1, dims=1), dim=1) # (B,)
+    return -beta/2 * torch.sum(state * torch.roll(state, shifts=1, dims=1), dim=1) # (B,)
 
 def pncg_dist(state: torch.Tensor, alpha: float = 1.0, p: float = 1.0) -> torch.Tensor:
     assert state.grad is not None
     diffs = VOCAB - state.unsqueeze(-1) # (B, N, V)
-    means = (-1/2) * state.grad.unsqueeze(-1) * diffs # (B, N, V)
-    regs = (-1/(2*alpha)) * torch.norm(diffs.unsqueeze(-1), p=p, dim=-1) # (B, N, V)
+    means = -1/2 * state.grad.unsqueeze(-1) * diffs # (B, N, V)
+    regs = -1/(2*alpha) * torch.norm(diffs.unsqueeze(-1), p=p, dim=-1) # (B, N, V)
     return F.log_softmax(means + regs, dim=2) # (B, N, V)
 
 def pncg_sample(prop_dist: torch.Tensor) -> torch.Tensor:
-    B, N, V = prop_dist.shape
+    B, N, _ = prop_dist.shape
     flat_dist = prop_dist.reshape(-1, V) # (BN, V)
     flat_indices = torch.multinomial(torch.exp(flat_dist), num_samples=1).squeeze(-1) # (BN,)
     return VOCAB.squeeze()[flat_indices.reshape(B, N)] # (B, N)
@@ -36,9 +42,29 @@ def mh_accept(
     accept_prob = torch.clamp(torch.exp(sample_energy + state_prob - state_energy - sample_prob), max=1)
     return torch.rand_like(accept_prob) < accept_prob
 
+def state_to_index(state: torch.Tensor) -> List[int]:
+    B, N = state.shape
+    base = VOCAB.shape[-1]
+    acc = torch.zeros(B, device=device, dtype=torch.long)
+    index = (state.unsqueeze(-1) == VOCAB).long().argmax(dim=-1, keepdim=False) # (B, N)
+    for i in range(N):
+        acc += index[:, i] * (base ** i)
+    return acc.tolist()
+
+def compute_exact_dist(n: int, beta: float):
+    energy = torch.zeros(2**n, dtype=torch.float32, device='cpu')
+    for _state in product(VOCAB.squeeze().tolist(), repeat=n):
+        state = torch.tensor([_state], device='cpu')
+        energy[state_to_index(state)[0]] = ncycle_energy(state, beta=beta)[0].item()
+    return torch.softmax(energy, dim=0)
+
+def tvd(exact_dist: torch.Tensor, empirical_dist: torch.Tensor):
+    return 1/2 * torch.sum(torch.abs(exact_dist - empirical_dist)).item()
+
 if __name__ == '__main__':
     ALPHA, BETA, P = 1.0, 0.42, 1.0
-    B, N = 1, 5
+    B, N = 1, 10
+    STEPS = 5000
 
     generator = torch.Generator(device=device)
     generator.manual_seed(42)
@@ -52,7 +78,11 @@ if __name__ == '__main__':
     state.requires_grad_(True)
 
     total_accepted = 0
-    for i in range(500):
+    exact_dist = compute_exact_dist(N, BETA)
+    empirical_dist = torch.zeros(2**N, dtype=torch.float32, device='cpu')
+    tvds = []
+
+    for i in range(STEPS):
         state_energy = ncycle_energy(state, beta=BETA)
         state_energy.sum().backward()
 
@@ -82,7 +112,11 @@ if __name__ == '__main__':
                 state.detach().clone()
             )
             state.requires_grad_(True)
+            assert state.grad is None
 
-        if (i+1) % 100 == 0:
-            print('energy', state_energy)
-            print('acceptance rate', total_accepted / ((i+1) * B))
+        empirical_dist[state_to_index(state)[0]] += 1
+        tvds.append(tvd(exact_dist, empirical_dist / empirical_dist.sum()))
+
+    plt.plot(np.arange(STEPS), tvds)
+    plt.yticks(np.arange(0, 1 + 0.05, 0.05))
+    plt.show()
