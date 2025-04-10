ad3ae339eb0449f4a58b7c7abdab3751f2a5a2dd
TJ Bai <tjianbai@gmail.com>
2025-04-09 23:46:42 -0400

    working mtm i think need to tune step size and then find divergences

--- forge/ising.py
+++ forge/ising.py
@@ -1,11 +1,14 @@
+import time
 from typing import List
 from itertools import product
 
 import torch
 import torch.nn.functional as F
+import fire
 import numpy as np
 import matplotlib.pyplot as plt
-import fire
+import seaborn as sns
+from tqdm import tqdm
 
 # TODO -- reproducible preamble
 
@@ -23,13 +26,22 @@ def pncg_dist(state: torch.Tensor, alpha: float = 1.0, p: float = 1.0) -> torch.
     regs = -1/(2*alpha) * torch.norm(diffs.unsqueeze(-1), p=p, dim=-1) # (B, N, V)
     return F.log_softmax(means + regs, dim=2) # (B, N, V)
 
-def pncg_sample(prop_dist: torch.Tensor) -> torch.Tensor:
+def pncg_sample(prop_dist: torch.Tensor, k: int = 1) -> torch.Tensor:
     B, N, _ = prop_dist.shape
     flat_dist = prop_dist.reshape(-1, V) # (BN, V)
-    flat_indices = torch.multinomial(torch.exp(flat_dist), num_samples=1).squeeze(-1) # (BN,)
-    return VOCAB.squeeze()[flat_indices.reshape(B, N)] # (B, N)
+    if k == 1:
+        flat_indices = torch.multinomial(torch.exp(flat_dist), num_samples=1).squeeze(-1) # (BN,)
+        return VOCAB.squeeze()[flat_indices.reshape(B, N)] # (B, N)
+    else:
+        flat_indices = torch.multinomial(torch.exp(flat_dist), num_samples=k, replacement=True) # (BN, K)
+        flat_indices = flat_indices.view(B, N, k).permute(0, 2, 1) # (B, N, K) -> (B, K, N)
+        return VOCAB.squeeze()[flat_indices] # (B, K, N)
 
 def prop_prob(state: torch.Tensor, prop_dist: torch.Tensor):
+    if prop_dist.shape[0] < state.shape[0]:
+        prop_dist = prop_dist.expand(state.shape[0], *prop_dist.shape[1:])
+    elif prop_dist.shape[0] > state.shape[0]:
+        state = state.expand(prop_dist.shape[0], *state.shape[1:])
     index = (state.unsqueeze(-1) == VOCAB).long().argmax(dim=-1, keepdim=True) # (B, N, 1)
     probs =  torch.gather(prop_dist, dim=2, index=index) # (B, N)
     return torch.sum(probs, dim=1).squeeze(-1) # (B,)
@@ -42,7 +54,7 @@ def mh_accept(
     sample_energy: torch.Tensor,
     sample_prob: torch.Tensor,
 ) -> torch.Tensor:
-    accept_prob = torch.clamp(torch.exp(sample_energy + state_prob - state_energy - sample_prob), max=1)
+    accept_prob = torch.clamp(torch.exp(state_energy + state_prob - sample_energy - sample_prob), max=1)
     return torch.rand_like(accept_prob) < accept_prob
 
 def state_to_index(state: torch.Tensor) -> List[int]:
@@ -56,25 +68,17 @@ def state_to_index(state: torch.Tensor) -> List[int]:
 
 def compute_exact_dist(n: int, beta: float):
     energy = torch.zeros(2**n, dtype=torch.float32, device='cpu')
-    for _state in product(VOCAB.squeeze().tolist(), repeat=n):
+    for _state in tqdm(product(VOCAB.squeeze().tolist(), repeat=n), desc='computing exact distribution', total=2**n):
         state = torch.tensor([_state], device='cpu')
         energy[state_to_index(state)[0]] = ncycle_energy(state, beta=beta)[0].item()
-    return torch.softmax(energy, dim=0)
+    return F.softmax(-energy, dim=0)
 
 def tvd(exact_dist: torch.Tensor, empirical_dist: torch.Tensor):
     return 1/2 * torch.sum(torch.abs(exact_dist - empirical_dist)).item()
 
-def run_pncg(
-    alpha: float = 1.0,
-    beta: float = 0.42,
-    p: float = 1.0,
-    bsz: int = 1,
-    seqlen: int = 5,
-    steps: int = 500,
-):
+def init_state(bsz: int, seqlen: int, seed: int):
     generator = torch.Generator(device=device)
     generator.manual_seed(42)
-
     state =  torch.randint(
         0, 2, (bsz, seqlen),
         device=device,
@@ -83,15 +87,119 @@ def run_pncg(
     )
     state = 2 * state - 1 # (0, 1) -> (-1, 1)
     state.requires_grad_(True)
+    return state
+
+def run_mtm_pncg(
+    alpha: float = 1.0,
+    beta: float = 0.42,
+    p: float = 1.0,
+    num_samples: int = 4,
+    seqlen: int = 5,
+    steps: int = 500,
+    seed: int = 42,
+):
+    assert num_samples > 1
+    x = init_state(1, seqlen, seed) # too lazy to do bsz > 1 and num_samples > 1
 
     total_accepted = 0
     exact_dist = compute_exact_dist(seqlen, beta)
     empirical_dist = torch.zeros(2**seqlen, dtype=torch.float32, device='cpu')
     tvds = []
 
-    for i in range(steps):
-        if state.grad:
-            state.grad.zero_()
+    s = time.time()
+    for i in tqdm(range(steps)):
+        assert x.grad is None
+        x_energy = ncycle_energy(x, beta=beta)
+        x_energy.sum().backward()
+
+        with torch.no_grad():
+            prop_dist = pncg_dist(x, alpha=alpha, p=p)
+            ys = pncg_sample(prop_dist, k=num_samples).squeeze(0) # (K, N)
+
+        ys = ys.detach().clone().requires_grad_(True)
+        y_energies = ncycle_energy(ys, beta=beta) # (K,)
+        y_energies.sum().backward()
+
+        with torch.no_grad():
+            prop_dist = pncg_dist(ys, alpha=alpha, p=p) # (K, N, V)
+            txy = prop_prob(x, prop_dist) # (K,)
+            log_forward_weights = txy - y_energies
+
+        selected_idx = torch.multinomial(
+            F.softmax(log_forward_weights, dim=0),
+            num_samples=1
+        ).item()
+
+        yk = ys[selected_idx].unsqueeze(0).detach().clone().requires_grad_(True) # (1, N)
+        yk.grad = ys.grad[0].unsqueeze(0).detach().clone()
+
+        with torch.no_grad():
+            yk_prop_dist = pncg_dist(yk, alpha=alpha, p=p)
+            ref_xs = pncg_sample(yk_prop_dist, k=num_samples-1).squeeze(0) # (K-1, N)
+
+        ref_set = torch.cat((x.detach(), ref_xs), dim=0).requires_grad_(True)
+        ref_set_energies = ncycle_energy(ref_set, beta=beta)
+        ref_set_energies.sum().backward()
+
+        with torch.no_grad():
+            prop_dist = pncg_dist(ref_set, alpha=alpha, p=p)
+            tyx = prop_prob(yk, prop_dist)
+            log_reverse_weights = tyx - ref_set_energies
+
+        log_accept_ratio = torch.logsumexp(log_forward_weights, dim=0) - torch.logsumexp(log_reverse_weights, dim=0)
+        accept_prob = torch.clamp(torch.exp(log_accept_ratio), max=1.0)
+
+        if torch.rand(1, device=device) < accept_prob:
+            x = yk.detach().clone().requires_grad_(True)
+            total_accepted += 1
+        else:
+            x = x.detach().clone().requires_grad_(True)
+
+        empirical_dist[state_to_index(x)[0]] += 1
+        tvds.append(tvd(exact_dist, empirical_dist / empirical_dist.sum()))
+
+    sns.set_theme(style='whitegrid')
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
+
+    indices = np.arange(len(exact_dist))
+    ax1.plot(indices, exact_dist, linewidth=0.5)
+    ax1.fill_between(indices, 0, exact_dist, alpha=0.3)
+    ax1.set_title('Exact Distribution')
+
+    ax2.plot(indices, empirical_dist, linewidth=0.5)
+    ax2.fill_between(indices, 0, empirical_dist, alpha=0.3)
+    ax2.set_title('Empirical Distribution')
+
+    ax3.plot(np.arange(steps), tvds)
+    ax3.grid(axis='y', linestyle='--', alpha=0.7)
+    ax3.set_ylim(0, 0.5)
+    ax3.set_yticks(np.arange(0, 0.5 + 0.05, 0.05))
+    ax3.set_title('TVD')
+
+    wallclock = time.time() - s
+    fig.text(0.05, 0.05, f'n={seqlen}, steps={steps}, runtime={wallclock:.3f}s, sps={steps / wallclock:.3f}, accepted={total_accepted / steps:.2f}')
+    plt.tight_layout(pad=3)
+    plt.show()
+
+def run_pncg(
+    alpha: float = 1.0,
+    beta: float = 0.42,
+    p: float = 1.0,
+    bsz: int = 1,
+    seqlen: int = 5,
+    steps: int = 500,
+    seed: int = 42,
+):
+    state = init_state(bsz, seqlen, 42)
+
+    total_accepted = 0
+    exact_dist = compute_exact_dist(seqlen, beta)
+    empirical_dist = torch.zeros(2**seqlen, dtype=torch.float32, device='cpu')
+    tvds = []
+
+    s = time.time()
+    for i in tqdm(range(steps)):
+        assert state.grad is None
         state_energy = ncycle_energy(state, beta=beta)
         state_energy.sum().backward()
 
@@ -126,9 +234,29 @@ def run_pncg(
         empirical_dist[state_to_index(state)[0]] += 1
         tvds.append(tvd(exact_dist, empirical_dist / empirical_dist.sum()))
 
-    plt.plot(np.arange(steps), tvds)
-    plt.yticks(np.arange(0, 1 + 0.05, 0.05))
+    sns.set_theme(style='whitegrid')
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4))
+
+    indices = np.arange(len(exact_dist))
+    ax1.plot(indices, exact_dist, linewidth=0.5)
+    ax1.fill_between(indices, 0, exact_dist, alpha=0.3)
+    ax1.set_title('Exact Distribution')
+
+    ax2.plot(indices, empirical_dist, linewidth=0.5)
+    ax2.fill_between(indices, 0, empirical_dist, alpha=0.3)
+    ax2.set_title('Empirical Distribution')
+
+    ax3.plot(np.arange(steps), tvds)
+    ax3.grid(axis='y', linestyle='--', alpha=0.7)
+    ax3.set_ylim(0, 0.5)
+    ax3.set_yticks(np.arange(0, 0.5 + 0.05, 0.05))
+    ax3.set_title('TVD')
+
+    wallclock = time.time() - s
+    fig.text(0.05, 0.05, f'n={seqlen}, steps={steps}, runtime={wallclock:.3f}s, sps={steps / wallclock:.3f}, accepted={total_accepted / steps:.2f}')
+    plt.tight_layout(pad=3)
     plt.show()
 
 if __name__ == '__main__':
-    fire.Fire(run_pncg)
+    # fire.Fire(run_pncg)
+    fire.Fire(run_mtm_pncg)
